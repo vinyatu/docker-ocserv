884651575f417fe2102a89a9fff490c2cc66b13a
Ameir Abdeldayem <ameirh@gmail.com>
2024-06-06 12:53:51 -0400

    Add network to iptables command
    When running in some environments, e.g. Kubernetes, the iptables command can cause the CNI to have issues. In my case, both calico-typha and calico-node began to fail health checks once the original iptables command was executed. Adding the ocserv network to the command allowed it to work as expected.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -63,7 +63,7 @@ fi
 sysctl -w net.ipv4.ip_forward=1
 
 # Enable NAT forwarding
-iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -t nat -A POSTROUTING -j MASQUERADE -s 192.168.99.0/24
 iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 
 # Enable TUN device
